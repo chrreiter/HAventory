19788e6cd49d859f01f1cf27a6df06081c2f9860
chrreiter <christoph.reiter@tum.de>
2026-01-18 22:16:19 +0100

    chore: delete overly verbose comments

--- tests/test_repository_search_offline.py
+++ tests/test_repository_search_offline.py
@@ -61,37 +61,8 @@ async def test_text_search_fuzzy_matching() -> None:
     repo = Repository()
     i1 = repo.create_item({"name": "Battery AA"})
 
-    # "Battery" (typo) - trigrams should overlap enough
-    # Trigrams for "Battery": bat, att, tte, ter, ery
-    # Trigrams for "Battery": bat, ate, ter, ery
-    # Intersection: bat, ter, ery (3 common trigrams)
-
-    # Our simple logic in repository is currently strict:
-    # "Intersection of all trigrams in the word" => so if I type "Battery",
-    # it generates [bat, ate, ter, ery].
-    # "ate" is NOT in Battery. So strict intersection will fail.
-    # Wait, let's re-read the implementation plan/code.
-    # Code says: "matches.update(fuzzy_matches)" where fuzzy_matches is intersection of
-    # candidate sets for each trigram of the QUERY word.
-    #
-    # So if Query="Battery", trigrams=[bat, ate, ter, ery].
-    # Items containing "bat": {Battery}
-    # Items containing "ate": {} (unless description has it)
-    # Items containing "ter": {Battery}
-    # Items containing "ery": {Battery}
-    # Intersection({Battery}, {}, {Battery}, {Battery}) -> {}
-    #
-    # So actually, my current simple implementation requires ALL trigrams of the query
-    # to be present in the target. This supports partial matches (substrings) effectively,
-    # but NOT typos where a trigram is mutated.
-    # Typos require "At least X% of trigrams match".
-    #
-    # Let's adjust expectation: The current implementation as coded supports *substring search*
-    # via trigrams (e.g. searching for "tte" finding "Battery"), but maybe not full typos
-    # if the typo breaks a trigram.
-    #
-    # Let's test "substring" capability which is what that logic actually enables effectively
-    # if I type a partial word that isn't a prefix. E.g. "atter".
+    # Substring search via trigrams (not a prefix)
+    # "atter" exists within "Battery"
 
     # "atter" -> att, tte, ter. All are in "Battery".
     results = repo.list_items(flt={"q": "atter"})["items"]
